5a046fb694314a252dace800f8e0e8bef6f403c5
dony79 <dony79@icloud.com>
2020-09-16 14:35:00 +0000

    review1

---
+++ src/main/java/awslv2flower/external/ReviewService.java
@@ -0,0 +1,17 @@
+
+package awslv2flower.external;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Date;
+
+@FeignClient(name="review", url="http://review:8080")
+public interface ReviewService {
+
+    @RequestMapping(method= RequestMethod.POST, path="/reviews")
+    public void reviewRequest(@RequestBody Review review);
+
+}
